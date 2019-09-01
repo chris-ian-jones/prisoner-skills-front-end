066ed733b36e16593fa8001a0e03125a06e0bc5f
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-01 03:14:10 +0100

    WIP: setup scaffolding for <PrisonerSkillCard />

--- src/components/PrisonerSkillCard.js
+++ src/components/PrisonerSkillCard.js
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 
 const PrisonerSkillCard = props => {
+  const [prisonerData, setPrisonerData] = useState(null)
+  const prisonerId = parseInt(props.match.params.id)
+  console.log(prisonerData)
+  
+  useEffect(() => {
+    Axios
+      .get(`https://prisoner-skills-cj.herokuapp.com/api/prisoners/${prisonerId}/skills`)
+      .then(result => {
+        console.log('axios get skills result: ', result)
+        setPrisonerData(result.data)
+      })
+      .catch(error => {
+        console.log('axios get skills error: ', error)
+      })
+  }, [])
+
   return (
-    <p>prisonerskillcard placeholder</p>
+    <>
+      {prisonerData ? 
+        <div>
+          <p>Name: {prisonerData.name}</p>
+          <p>Gender: {prisonerData.gender}</p>
+          <p>Can Have Work Leave: {prisonerData.canHaveWorkLeave ? 'Yes' : 'No'}</p>
+          <p>Skills:</p>
+          {prisonerData.skills.map(skill => <p>{skill.name}</p>)}
+        </div>
+      :
+      ''
+      }
+    </>
   )
 }
 
