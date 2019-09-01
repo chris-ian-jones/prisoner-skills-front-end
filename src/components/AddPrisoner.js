294fef56be8b9135738b241d866a2e4619f54966
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-01 23:13:29 +0100

    WIP: fixed dropdown select bug on <AddPrisoner /> form

--- src/components/AddPrisoner.js
+++ src/components/AddPrisoner.js
@@ -1,14 +1,18 @@
 import React, {useState} from 'react'
 
+import axiosWithAuth from './../utils/axiosWithAuth'
+import history from './../utils/history'
+
 const AddPrisoner = () => {
   const [prisonerInfo, setPrisonerInfo] = useState(
     {
       name: '',
-      gender: '',
-      canHaveOutsideClearence: '',
+      gender: 'Male',
+      canHaveWorkLeave: 'Yes',
       skills: ''
     }
   )
+  const adminId = parseInt(localStorage.getItem("adminId"))
 
   const onChangeHandler = event => {
     setPrisonerInfo(
@@ -22,6 +26,39 @@ const AddPrisoner = () => {
   const onSubmitHandler = event => {
     event.preventDefault()
     console.log(prisonerInfo)
+    
+    const newPrisoner = {
+      name: prisonerInfo.name,
+      gender: prisonerInfo.gender,
+      canHaveWorkLeave: prisonerInfo.canHaveWorkLeave,
+      prison_id: adminId
+    }
+
+    console.log(newPrisoner)
+
+    axiosWithAuth()
+      .post("https://prisoner-skills-cj.herokuapp.com/api/auth/prisoners", newPrisoner)
+      .then(result => {
+        console.log('axios create prisoner result: ', result)
+        
+        const prisonerSkills = {
+          name: prisonerInfo.skills,
+          prisoner_id: result.data.id
+        }
+      
+        axiosWithAuth()
+          .post("https://prisoner-skills-cj.herokuapp.com/api/auth/skills", prisonerSkills)
+          .then(result => {
+            console.log('axios add prisoner skills result: ', result)
+            history.push(`/admin/prison/${adminId}`)
+          })
+          .catch(error => {
+            console.log('axios add prisoner skills error: ', error)
+          })
+      })
+      .catch(error => {
+        console.log('axios create prisoner error: ', error)
+      })
   }
 
   return (
@@ -29,13 +66,13 @@ const AddPrisoner = () => {
       <input name='name' value={prisonerInfo.name} onChange={onChangeHandler} placeholder='Name' />
       <p><strong>Select Gender:</strong></p>
       <select name='gender' value={prisonerInfo.gender} onChange={onChangeHandler}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
+          <option value="Male" selected>Male</option>
+          <option value="Female">Female</option>
       </select>
       <p><strong>Can Have Work Leave:</strong></p>
-      <select name='canHaveOutsideClearence' value={prisonerInfo.canHaveOutsideClearence} onChange={onChangeHandler}>
-          <option value="yes">Yes</option>
-          <option value="no">No</option>
+      <select name='canHaveWorkLeave' value={prisonerInfo.canHaveWorkLeave} onChange={onChangeHandler}>
+          <option value="1" selected>Yes</option>
+          <option value="0">No</option>
       </select>
       <input name='skills' value={prisonerInfo.skills} onChange={onChangeHandler} placeholder='Skills' />
       <button>Add Prisoner</button>
